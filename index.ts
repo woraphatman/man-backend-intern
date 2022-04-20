dc0994a8c93b63709df075c00b24dbbfa9d63a7d
woraphatman <81944147+woraphatman@users.noreply.github.com>
2022-04-20 15:29:04 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import  Router  from "./routes/router";
 const express = require("express");
 const app = express();
 const port = 3001;
-
+const movie = require('./movie')
 app.use(express.json());
 
 app.listen(port, () => {
