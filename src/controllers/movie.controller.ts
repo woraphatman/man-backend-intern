1dc17c0725090da50712a26a28519e2f73fe16d4
woraphatman <81944147+woraphatman@users.noreply.github.com>
2022-04-21 16:56:47 +0700

    update

--- src/controllers/movie.controller.ts
+++ src/controllers/movie.controller.ts
@@ -3,7 +3,10 @@ const movie = require ("../dto/db")
 
 export function get (req:Request, res: Response)  {
     console.log(req.headers)
+    res.status(400)
   res.json(movie)
+  
+  
   }
   
   export function getID (req: Request, res: Response) {
@@ -13,7 +16,7 @@ export function get (req:Request, res: Response)  {
   export function create(req: Request, res: Response) {
     movie.push(req.body)
     console.log(req.headers.ss)
-    if(req.headers.ss != "user1"){
+    if(req.headers.Accept != "user1"){
         return res.json({ error:"You must be Admin"}
         )
   }
@@ -21,12 +24,16 @@ export function get (req:Request, res: Response)  {
     status: "ok",
     message: "Create Finish",
     user: req.body
-  });}
+  })}
   
   export function put(req: Request, res: Response)  {
     const updateIndex = movie.findIndex(movie => movie.id === req.params.id)
     res.json(Object.assign(movie[updateIndex], req.body))
-  }
+    res.status(202).send({
+      status: "ok",
+      message: "Create Finish",
+      user: req.body
+    })}
   
   export function deleteid (req: Request, res: Response)  {
     const deleteIndex = movie.findIndex(movie => movie.id === req.params.id)
